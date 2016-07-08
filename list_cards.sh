52384681cf1180f25d5ef7d8bdffcfd07db59560
Rajan <rajan@pressplaytv.in>
2016-07-08 11:14:27 +0530

    uploading xerox to git

--- list_cards.sh
+++ list_cards.sh
@@ -5,5 +5,10 @@ set -e
 ROOTDRIVE=`mount | grep 'on / ' | awk {'print $1'} |  cut -c6-8`
 #LISTCARDS=`df -h | awk '{print $1}' | grep  "/dev" | grep -v $ROOTDRIVE | cut -c1-8 | sort | uniq`
 
-LISTCARDS=`sudo fdisk -l | grep "Disk" | grep "/dev/" |  awk '{print $2}' | grep -v $ROOTDRIVE | tr -d ':' | sort | uniq`
-echo $LISTCARDS 
+SHOWCARDS=`sudo fdisk -l | grep "Disk" | grep "/dev/" |  awk '{print $2,$3,$4}' | grep -v $ROOTDRIVE | tr -d ' ,' | sort | uniq`
+#LISTCARDS=`sudo fdisk -l | grep "Disk" | grep "/dev/" |  awk '{print $2}' | grep -v $ROOTDRIVE | tr -d ' ,' | sort | uniq`
+
+echo $SHOWCARDS
+#echo $LISTCARDS 
+
+
