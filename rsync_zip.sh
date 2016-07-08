74380fc226b5984d431cc7425b8a59c0a9d1eb74
Rajan <rajan@pressplaytv.in>
2016-07-08 11:20:25 +0530

    latest rsync_zip upload

--- rsync_zip.sh
+++ rsync_zip.sh
@@ -3,15 +3,15 @@
 set -e
 
 LOCATION="/root/First-Ride-master/"
-echo " " > "$LOCATION"rsync_status.log
 if [ ! -f "$LOCATION"request.lock ];then
 	
-    ENDPOINT=`curl -s http://devmeet.dev.pressplaytv.in/util/box-version/latest`
+    ENDPOINT=`curl -s http://ammunition.pressplaytv.in/util/box-version/latest`
     STATUS=`echo $ENDPOINT | jq -r '.process_status'`
     ROW_ID=`echo $ENDPOINT | jq -r '.row_id'`
 
 
     if [ $STATUS == "success" ];then
+	echo "" > "$LOCATION"rsync_status.log
 
         touch "$LOCATION"request.lock
         echo "Zip File created successfully" >> "$LOCATION"rsync_status.log
@@ -20,12 +20,12 @@ if [ ! -f "$LOCATION"request.lock ];then
         ZIP_FILE=`echo $ZIP_LOCATION"complete_content.zip"`
         echo "zip file : " $ZIP_FILE >> "$LOCATION"rsync_status.log
         echo "rsync initiated" >> "$LOCATION"rsync_status.log
-        rsync --progress -rave "ssh -i /root/First-Ride-master/devmeet" devmeet@54.255.151.235:$ZIP_FILE "$LOCATION"zipfolder >> "$LOCATION"status.lock
+        rsync --progress -rave "ssh -i /root/First-Ride-master/ammunition.pem" ubuntu@52.74.116.76:$ZIP_FILE "$LOCATION"zipfolder >> "$LOCATION"status.lock
         echo "rsync completed" >> "$LOCATION"rsync_status.log
         rm -rf "$LOCATION"request.lock
 
 	# CURL REQUEST TO CHECK STATUS
-	curl -i http://devmeet.dev.pressplaytv.in/util/box-version/update?row_id=$ROW_ID
+	curl -i http://ammunition.pressplaytv.in/util/box-version/update?row_id=$ROW_ID
     else
 
         echo "mmll"
